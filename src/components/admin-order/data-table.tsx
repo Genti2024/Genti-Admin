28db1b2f4483ec7f90a3adab4a788ef032a2f210
SeungminCha <dominico1225@gmail.com>
2024-06-21 16:59:56 +0900

    feat: 담당자 수정 버튼
    -모달로 담당자 수정

--- src/components/admin-order/data-table.tsx
+++ src/components/admin-order/data-table.tsx
@@ -7,12 +7,22 @@ import {
   getPaginationRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { ListFilter } from 'lucide-react'
+import { ListFilter, Pencil } from 'lucide-react'
 import { useState } from 'react'
 
 import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 interface DataTableProps<TData, TValue> {
@@ -61,6 +71,33 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
             <DropdownMenuItem onClick={() => handleStatusFilter('NOT STARTED')}>작업완료</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">
+              <Pencil className="w-4 h-4 mr-2" />
+              담당자 수정
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>담당자 수정 및 입력</DialogTitle>
+              <DialogDescription>
+                {table.getFilteredSelectedRowModel().rows.length} 개의 행이 선택되었습니다.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid items-center grid-cols-4 gap-4">
+                <Label htmlFor="name" className="text-right">
+                  이름
+                </Label>
+                <Input id="name" defaultValue="익명의 담당자" className="col-span-3" />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="border rounded-md">
         <Table>
